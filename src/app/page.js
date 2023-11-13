76f41a5f1ab3bbf438063c51a9664ca1b36a15a6
jinhomun <answlsgh95@gmail.com>
2023-11-13 11:10:49 +0900

    2023.11.13 next.js 1차

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from 'react'
 import Skip from '@/components/Skip'
 import Header from '@/components/Header'
 import Intro from '@/components/Intro'
@@ -7,9 +8,16 @@ import Site from '@/components/Site'
 import Port from '@/components/Port'
 import Contact from '@/components/Contact'
 import Footer from '@/components/Footer'
+import smooth from '@/utils/smooth'
+import link from '@/utils/link'
+
 
 
 export default function Home() {
+  useEffect(() => {
+    smooth()
+    link()
+  }, [])
   return (
     <>
       <Skip />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { headerNav } from '../constants'
 
 const Header = () => {
+
+    const [show, setshow] = useState(false);
+
+    const toggleMenu = () => {
+        setshow((prevShow) => !prevShow);
+    }
+
     return (
-        <div>Header</div>
+        <header id="header" role="banner">
+            <div className="header__inner">
+                <div className="header__logo">
+                    <a href="/">Zino <em>PORTFOLIO</em></a>
+                </div>
+                <div className={`header__nav ${show ? "show" : ""}`} role="navigation" aria-label="메인 메뉴">
+                    <ul>
+                        {headerNav.map((nav, key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div
+                    className="header__nav__mobile"
+                    id="headerToggle"
+                    aria-controls="primary-menu"
+                    aria-expanded={show ? "false" : "true"}
+                    role="button"
+                    onClick={toggleMenu}
+                >
+                    <span></span>
+                </div>
+            </div>
+        </header>
     )
 }
 
